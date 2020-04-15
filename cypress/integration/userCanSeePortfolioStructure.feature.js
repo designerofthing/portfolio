c7769aee429013fed3eb602d6ff3896901647c65
designerofthing <designerofthings@gmail.com>
2020-04-15 11:26:41 +0200

    adding semantic UI to index.html

--- cypress/integration/userCanSeePortfolioStructure.feature.js
+++ cypress/integration/userCanSeePortfolioStructure.feature.js
@@ -2,7 +2,7 @@ describe('Portfolio interface', () => {
     it('successfully renders', () => {
         cy.visit('http://localhost:3000');
         cy.get('#header').should('contain', 'My Portfolio');
-        cy.get('#footer').should('contain', 'Made with React 16.12.0');
-        cy.get('#hello').should('contain', 'Hello World');
+        cy.get('#footer').should('contain', 'Made with React 16.13.1');
+        cy.get('#hello').should('contain', 'Howdy, I can write what ever I want here');
     })
 })
